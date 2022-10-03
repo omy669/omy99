0aafff7b85156b11a5debc60fecabe589d90865d
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-04 04:28:25 +0800

    Update ouou.js

--- ouou.js
+++ ouou.js
@@ -1,19 +1,34 @@
-[rewrite_local]
-^https:\/\/apiios_dq\.xiangha\.com\/v4\/User url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js
-[mitm] 
-hostname = dq.xiangha.com
-
-
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-
-const ouou = '/getUserData';
-
-if (url.indexOf(ouou) != -1) {
-    obj.vip.is_open_vip = 1;
-obj.vip.is_vip = 2;
-    body = JSON.stringify(obj);
+obj = {
+  "msg" : "成功",
+  "data" : {
+    "TestType" : "4301",
+    "MembersBeginDateMs" : null,
+    "RegisterTs" : "1664790001697",
+    "GzhOpenId" : null,
+    "AppleId" : null,
+    "Sex" : 0,
+    "Integral" : 0,
+    "WxOpenId" : null,
+    "Mobile" : null,
+    "NickName" : "My.",
+    "UnionId" : "o5YYFwHiYNN8K4taZ_dR5pNA7VU0",
+    "DiscountEnd2" : null,
+    "VIP" : 1,
+    "DiscountEnd1" : null,
+    "DeviceToken" : null,
+    "ID" : 2374535,
+    "SNSPlatform" : "WX",
+    "Photo" : "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83epibiczGkwFxxEtIJgYJtg9Qnp0Ojjb0icD0YfyiaHbxyu1hIr9V93tGjTJAiamNfOR0daCraMJwH7mBAA/132",
+    "TrialEndDateMs" : null,
+    "OpenId" : "onAxy5__m7iaf-xODGFpbrtrIJwE",
+    "Amount" : "0.00",
+    "InviteCode" : null,
+    "MembersEndDateMs" : null
+  },
+  "code" : "10000"
 }
-
+    body = JSON.stringify(obj);
 $done({body});
