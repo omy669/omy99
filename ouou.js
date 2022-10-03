5da741bdf149bd527404272d45f1e2e40713ba41
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-03 17:53:24 +0800

    Update ouou.js

--- ouou.js
+++ ouou.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/integral\.dasyibalang\.com url script-response-body ouou.js
+^https:\/\/integral\.dasyibalang\.com url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js.js
 [mitm] 
 hostname = integral.dasyibalang.com
 
@@ -10,7 +10,7 @@ var obj = JSON.parse(body);
 const ouou = '/User';
 
 if (url.indexOf(ouou) != -1) {
-    obj.data.VIP =1;
+    obj.data.VIP = 1;
     body = JSON.stringify(obj);
 }
 $done({body});
