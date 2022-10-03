302dd43b8dd63077e3f34c5dfd5df05eacddbb2e
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-04 02:23:23 +0800

    Update ouou.js

--- ouou.js
+++ ouou.js
@@ -1,16 +1,19 @@
 [rewrite_local]
-^https:\/\/integral\.dasyibalang\.com url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js
+^https:\/\/apiios_dq\.xiangha\.com\/v4\/User url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js
 [mitm] 
-hostname = integral.dasyibalang.com
+hostname = dq.xiangha.com
+
 
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const ouou = '/User';
+const ouou = '/getUserData';
 
 if (url.indexOf(ouou) != -1) {
-    obj.data.VIP = 1;
+    obj.vip.is_open_vip = 1;
+obj.vip.is_vip = 2;
     body = JSON.stringify(obj);
 }
+
 $done({body});
