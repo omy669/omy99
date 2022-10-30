2ab150a03cbd0fd080c443ea66c98c19e27f6a09
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-30 17:49:30 +0800

    Update ouou.js

--- ouou.js
+++ ouou.js
@@ -1,34 +1,13 @@
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-obj = {
-  "msg" : "成功",
-  "data" : {
-    "TestType" : "4301",
-    "MembersBeginDateMs" : null,
-    "RegisterTs" : "1664790001697",
-    "GzhOpenId" : null,
-    "AppleId" : null,
-    "Sex" : 0,
-    "Integral" : 0,
-    "WxOpenId" : null,
-    "Mobile" : null,
-    "NickName" : "My.",
-    "UnionId" : "o5YYFwHiYNN8K4taZ_dR5pNA7VU0",
-    "DiscountEnd2" : null,
-    "VIP" : 1,
-    "DiscountEnd1" : null,
-    "DeviceToken" : null,
-    "ID" : 2374535,
-    "SNSPlatform" : "WX",
-    "Photo" : "https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83epibiczGkwFxxEtIJgYJtg9Qnp0Ojjb0icD0YfyiaHbxyu1hIr9V93tGjTJAiamNfOR0daCraMJwH7mBAA/132",
-    "TrialEndDateMs" : null,
-    "OpenId" : "onAxy5__m7iaf-xODGFpbrtrIJwE",
-    "Amount" : "0.00",
-    "InviteCode" : null,
-    "MembersEndDateMs" : null
-  },
-  "code" : "10000"
-}
-    body = JSON.stringify(obj);
-$done({body});
+[rewrite_local]
+^http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/register|http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/set*? url script-response-body egdd.js
+
+
+[mitm]
+hostname = gateway.ergedd.com,gateway.ergedd.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/"vip_endtime\":\d+/g,'"vip_endtime":1919486813');
+body = body.replace(/"is_vip\":\d+/g,'"is_vip":1');
+body = body.replace(/"is_contract\":\d+/g,'"is_contract":1');
+$done(body);
