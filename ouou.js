9fb12c1e1d0e68c475b6a68318353ca9a9da5ef9
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-30 18:18:04 +0800

    Update ouou.js

--- ouou.js
+++ ouou.js
@@ -1,13 +1,2 @@
-[rewrite_local]
-^http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/register|http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/set*? url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js
-
-
-[mitm]
-hostname = gateway.ergedd.com,gateway.ergedd.com
-
 *******************************/
-var body=$response.body;
-body = body.replace(/"vip_endtime\":\d+/g,'"vip_endtime":1919486813');
-body = body.replace(/"is_vip\":\d+/g,'"is_vip":1');
-body = body.replace(/"is_contract\":\d+/g,'"is_contract":1');
-$done(body);
+[rewrite_local]^http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/register|http?:\/\/gateway\.ergedd\.com\/dduser\/user\/center\/set*?url script-response-body https://raw.githubusercontent.com/omy669/omy99/main/ouou.js[mitm]hostname = gateway.ergedd.com,gateway.ergedd.com*******************************/var body=$response.body;body = body.replace(/"vip_endtime\":\d+/g,'"vip_endtime":1919486813');body = body.replace(/"is_vip\":\d+/g,'"is_vip":1');body = body.replace(/"is_contract\":\d+/g,'"is_contract":1');$done（身体）；
