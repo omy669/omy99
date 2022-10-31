bbcb782cd7caa9fc4936dd2978dfb1e427741885
omy669 <83883902+omy669@users.noreply.github.com>
2022-10-31 16:31:15 +0800

    Update egdd.js

--- Personal_configuration/QuantumultX/egdd.js
+++ Personal_configuration/QuantumultX/egdd.js
@@ -4,4 +4,8 @@
 [mitm]
 hostname = gateway.ergedd.com,gateway.ergedd.com
 *******************************/
-var%20body%3D%24response.body%3B%0D%0Abody%20%3D%20body.replace%28%2F%22vip%5Fendtime%5C%22%3A%5Cd%2B%2Fg%2C%27%22vip%5Fendtime%22%3A1919486813%27%29%3B%0D%0Abody%20%3D%20body.replace%28%2F%22is%5Fvip%5C%22%3A%5Cd%2B%2Fg%2C%27%22is%5Fvip%22%3A1%27%29%3B%0D%0Abody%20%3D%20body.replace%28%2F%22is%5Fcontract%5C%22%3A%5Cd%2B%2Fg%2C%27%22is%5Fcontract%22%3A1%27%29%3B%0D%0A%24done%28body%29%3B
+var body=$response.body;
+body = body.replace(/"vip_endtime\":\d+/g,'"vip_endtime":1919486813');
+body = body.replace(/"is_vip\":\d+/g,'"is_vip":1');
+body = body.replace(/"is_contract\":\d+/g,'"is_contract":1');
+$done(body);
